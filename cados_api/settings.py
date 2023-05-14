ed5afa276bd231bfa03f10509ca82c56cb6dc8c8
keshav <keshavsharma1196@gmail.com>
2023-05-14 16:05:05 +0530

    fixing line length errors in settings file

--- cados_api/settings.py
+++ cados_api/settings.py
@@ -52,7 +52,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
-
+V1 = "authentication.default_user_authentication_rule"
+V2 = "TokenObtainPairSerializer"
+V3 = "TokenRefreshSerializer"
+V4 = "TokenVerifySerializer"
+V5 = "TokenBlacklistSerializer"
+V6 = "serializers.TokenObtainSlidingSerializer"
+V7 = "serializers.TokenRefreshSlidingSerializer"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
@@ -74,7 +80,7 @@ SIMPLE_JWT = {
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
-    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    "USER_AUTHENTICATION_RULE": f"rest_framework_simplejwt.{V1}",
 
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
@@ -86,12 +92,12 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 
-    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
-    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
-    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
-    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
-    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
-    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+    "TOKEN_OBTAIN_SERIALIZER": f"rest_framework_simplejwt.serializers.{V2}",
+    "TOKEN_REFRESH_SERIALIZER": f"rest_framework_simplejwt.serializers.{V3}",
+    "TOKEN_VERIFY_SERIALIZER": f"rest_framework_simplejwt.serializers.{V4}",
+    "TOKEN_BLACKLIST_SERIALIZER": f"rest_framework_simplejwt.serializers.{V5}",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": f"rest_framework_simplejwt.{V6}",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": f"rest_framework_simplejwt.{V7}",
 }
 
 MIDDLEWARE = [
